918b959683940bbded8399d08f95d2cf12c88cb9
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2019-06-30 17:53:50 +0700

    firebase auth, vuex, etc

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+      'open-sans': '"Open Sans", sans-serif',
+      roboto: '"Roboto", sans-serif'
+    }
+  },
+  variants: {}
+}
