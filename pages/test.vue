ceddfbf05b5e1bb2d28250a39d4da8845958c1b5
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-03-02 18:11:59 +0700

    test env

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    tesss
+  </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line space-before-function-paren
+  created() {
+    // eslint-disable-next-line no-console
+    console.log(process.env.FIREBASE_AUTH_DOMAIN)
+  }
+}
+</script>
