5f14cdca3604ef132fc07f18218b3f730397f7f0
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-12-10 07:29:54 +0700

    pwa settings test

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,20 +74,19 @@ export default {
   ],
 
   pwa: {
-    // disable the modules you don't need
-    meta: false,
-    icon: false,
-    // if you omit a module key form configuration sensible defaults will be applied
-    // manifest: false,
-
+    icon: {
+      source: './static/icon.png'
+    },
+    manifest: {
+      name: 'Ngirit',
+      short_name: 'Ngirit',
+      theme_color: '#0D47A1'
+    },
     workbox: {
       importScripts: [
         // ...
         '/firebase-auth-sw.js'
-      ],
-      // by default the workbox module will not install the service worker in dev environment to avoid conflicts with HMR
-      // only set this true for testing and remember to always clear your browser cache in development
-      dev: false
+      ]
     }
   },
 
