c65815c52f20fa9361b2cbb67967e6128edcf053
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-12-10 09:58:56 +0700

    added nav drawer

--- components/headers/HeaderBar.vue
+++ components/headers/HeaderBar.vue
@@ -1,30 +1,26 @@
 <template>
-  <v-app-bar v-if="user" app color="primary" dark dense>
-    <v-toolbar-title>Ngirit</v-toolbar-title>
-    <v-spacer />
-    <v-btn color="white" icon="icon" @click.prevent="signOut">
-      <v-icon>mdi-logout</v-icon>
-    </v-btn>
-  </v-app-bar>
+  <div>
+    <v-app-bar v-if="user" app color="primary" dark dense>
+      <v-toolbar-title>Ngirit</v-toolbar-title>
+      <v-spacer />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <navigation-sidebar />
+    </v-navigation-drawer>
+  </div>
 </template>
 <script>
-import { mapMutations, mapState } from 'vuex'
+import { mapState } from 'vuex'
 export default {
   name: 'HeaderBar',
+  data: () => ({
+    drawer: false
+  }),
   computed: {
     ...mapState({
       user: state => state.auth.user
     })
-  },
-  methods: {
-    ...mapMutations({
-      reset: 'auth/reset'
-    }),
-    signOut () {
-      this.$fire.auth.signOut()
-      this.reset()
-      this.$router.replace('/login')
-    }
   }
 }
 </script>
