8d305c0ba958cc036498eddaf97018b88a5e0a6e
gajayana <yosef@yudhawijaya.com>
2025-04-20 17:20:02 +0700

    Add initial Nuxt.js 3 setup with configuration files, README, and basic structure

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/scripts'
+  ]
+})
