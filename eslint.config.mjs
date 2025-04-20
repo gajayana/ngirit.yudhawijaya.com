39712bc258c0bcbf1605984f25a818c450513a48
gajayana <yosef@yudhawijaya.com>
2025-04-20 23:26:41 +0700

    update ESLint configuration to disable specific Vue rules for improved flexibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    rules: {
+      'vue/html-self-closing': 'off',
+      'vue/multi-word-component-names': 'off',
+    },
+  }
+);
